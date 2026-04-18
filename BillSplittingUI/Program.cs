9bbb2f152f7f641278930ca2bf9f17e68a4214bb
adamhuston <adam.huston@bsu.edu>
2026-04-18 00:30:25 -0400

    Implements UC4 + names QOL addition + performance screen

--- BillSplittingUI/Program.cs
+++ BillSplittingUI/Program.cs
@@ -9,14 +9,15 @@ var paymentRepository = new CsvPaymentRepository("payments.csv");
 var paymentHandler = new RecordPaymentHandler(paymentRepository, debtRepository);
 var billRepository = new CsvBillRepository("bills.csv");
 var billHandler = new CreateBillHandler(billRepository, debtRepository);
-
+var personRepository = new CsvPersonRepository("people.csv");   
 
 var screens = new Dictionary<string, IScreen>
 {
-    ["Record a Debt"] = new RecordDebtScreen(handler),
-    ["Record a Payment"] = new RecordPaymentScreen(paymentHandler, debtRepository),
-    ["Record a Bill"] = new RecordBillScreen(billHandler),
-    ["View Summaries"] = new ViewSummaryScreen()
+    ["Record a Debt"] = new RecordDebtScreen(handler, personRepository),
+    ["Record a Payment"] = new RecordPaymentScreen(paymentHandler, debtRepository, personRepository),
+    ["Record a Bill"] = new RecordBillScreen(billHandler, personRepository),
+    ["View Summaries"] = new ViewSummaryScreen(debtRepository, personRepository, paymentRepository, billRepository),
+    ["Test Performance"] = new TestPerformanceScreen(debtRepository, personRepository, paymentRepository, billRepository, "debts.csv", "people.csv", "payments.csv", "bills.csv")
 };
 
 var app = new App(screens, debtRepository);
